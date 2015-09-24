777b47b12cf96125e59d03922f1e706b4afae8e4
Tim Brailsford <tim.brailsford@nottingham.edu.my>
2015-09-24 17:38:53 +0800

    Update Main.java

--- src/Tpong/Main.java
+++ src/Tpong/Main.java
@@ -10,7 +10,6 @@ package Tpong;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Pong - v1");
 		MainWindow gameWindow = new MainWindow();
 		gameWindow.setVisible(true);
 		gameWindow.start();
