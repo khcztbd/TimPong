d24aa47612c9b9caa1d97e3caf96efcbe812a957
Tim Brailsford <tim.brailsford@nottingham.edu.my>
2015-09-24 17:25:36 +0800

    Added starting message

--- src/Tpong/Main.java
+++ src/Tpong/Main.java
@@ -10,6 +10,7 @@ package Tpong;
 public class Main {
 
 	public static void main(String[] args) {
+		System.out.println("Starting Pong...");
 		MainWindow gameWindow = new MainWindow();
 		gameWindow.setVisible(true);
 		gameWindow.start();
