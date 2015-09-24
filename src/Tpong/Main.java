ce79d810a3a53bca113be3c79e3225c774a2d85d
Tim Brailsford <tim.brailsford@nottingham.edu.my>
2015-09-24 17:16:21 +0800

    Tidied code

--- src/Tpong/Main.java
+++ src/Tpong/Main.java
@@ -10,10 +10,10 @@ package Tpong;
 public class Main {
 
 	public static void main(String[] args) {
-		   System.out.println("Staring Pong");
-	       MainWindow gameWindow = new MainWindow();
-	       gameWindow.setVisible(true);
-	       gameWindow.start();
+		System.out.println("Staring Pong"); // new message
+		MainWindow gameWindow = new MainWindow();
+		gameWindow.setVisible(true);
+		gameWindow.start();
 	}
 
 }
